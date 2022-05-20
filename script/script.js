f70efebf99b37fe8d5e4521011c938baabd9034a
luismariano <perezluismariano@hotmail.com>
2022-05-20 15:09:45 -0500

    css - update again

--- script/script.js
+++ script/script.js
@@ -12,13 +12,13 @@ const regexEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 function validateButton(){
     if (regexEmail.test(email.value)) {
         email.style.border = "1px solid hsl(223, 87%, 63%)";
-        output.style.display = "unset"
+        output.style.opacity = "100";
         output.style.color = "hsl(223, 87%, 63%)";
         output.innerText = "Done!";
     }
     else{
         email.style.border = "1px solid hsl(354, 100%, 66%)";
-        output.style.display = "unset"
+        output.style.opacity = "100";
         output.style.color = "hsl(354, 100%, 66%)";
         output.innerText = "Please provide a valid email adress";
     }   
